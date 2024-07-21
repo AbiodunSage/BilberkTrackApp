0670999246827b0b3c3bd6d0f4e0cf2b9377d063
Sani Omotola <noble.b.sage01@gmail.com>
2024-07-21 22:42:36 +0100

    environment variables

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -2,15 +2,18 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { config } from "dotenv";
+
+config();
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCdx6DdKPZaKW8iEvyCQm3zPYnbWoqfY6U",
-  authDomain: "bilberktraveltrackapp.firebaseapp.com",
-  projectId: "bilberktraveltrackapp",
-  storageBucket: "bilberktraveltrackapp.appspot.com",
-  messagingSenderId: "491798621058",
-  appId: "1:491798621058:web:51165008fb4ccb05817d9b",
-  measurementId: "G-EEJJ2RWEWK",
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 const app = initializeApp(firebaseConfig);
 
